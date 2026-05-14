a1fc28fcdcaccb282750341b77462526a4c6c9b5
Morgana <morgana.soares@aluno.fmpsc.edu.br>
2026-05-14 10:32:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Função chamada quando o usuário clica no botão, usa a funcionalidade pra criar putro texto
+function alterarTexto1() {
+
+    // Localiza o elemento com id="texto"
+    let paragrafo = document.getElementById("texto");
+
+    // Altera o conteúdo do parágrafo
+    paragrafo.innerHTML = "foi eu que alterei este texto!";
+}
+function alterarTexto2() {
+
+    // Localiza o elemento com id="texto"
+    let paragrafo = document.getElementById("texto");
+
+    // Altera o conteúdo do parágrafo
+    paragrafo.innerHTML = "nao vale nada nem le!";
+}
